7817d2d3efe807ad33e20ae759c175b583e317ee
Stefanos <stefanos6226@gmail.com>
2020-05-14 19:41:47 +0300

    writes testSubscriptionStore and testPriceNotNegative tests

--- tests/Unit/SubscriptionTest.php
+++ tests/Unit/SubscriptionTest.php
@@ -27,4 +27,36 @@ class SubscriptionTest extends TestCase
         $this->json('POST', 'api/subscriptions', $payload)
             ->assertStatus(400);
     }
+
+    public function testSubscriptionStore()
+    {
+        $payload = ['user_id' => 5,
+            'type' => 2,
+            'price' => 130,
+            'from' => '2020-10-10',
+            'to' => '2020-11-11']
+        ;
+        $this->json('POST', 'api/subscriptions', $payload)
+            ->assertStatus(200);
+        $response = $this->json('GET', 'api/subscriptions');
+        $response->assertJson(
+            ['user_id' => 5,
+                'type' => 2,
+                'price' => 130,
+                'from' => '2020-10-10',
+                'to' => '2020-11-11']
+        );
+    }
+
+    public function testPriceNotNegative()
+    {
+        $payload = ['user_id' => 5,
+            'type' => 2,
+            'price' => -130,
+            'from' => '2020-10-10',
+            'to' => '2020-11-11']
+        ;
+        $this->json('POST', 'api/subscriptions', $payload)
+            ->assertStatus(400);
+    }
 }
